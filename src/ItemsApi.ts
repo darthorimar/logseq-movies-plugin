febc0a72e44f707f2f33c4609ff8038d551956d7
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-07-30 20:28:12 +0200

    Refactor API parsing logic into standalone ResourceParsers

--- src/ItemsApi.ts
+++ src/ItemsApi.ts
@@ -1,40 +1,31 @@
 import {Item} from './Item'
 import {ItemChooser} from './ItemChooser'
-import {RateLimiter} from './RateLimiter'
+import {ResourceParserDefinition} from './ResourceParserDefinition'
+import {ResourceParser} from './ResourceParser'
 
 /**
  * Show `ItemChooser` and populate with results provided by some API
  *
  * @param title title for thing user is searching for, will be shown as input placeholder
  * @param icon input field icon
- * @param buildUrl build 3rd party API URL to search user query, retrieved URL should specify JSON as expected format
- * @param getCompletionItems handler to transform JSON API response to the list of items which will be shown in the completion box for the user
+ * @param parserDefinition describes the way to parse 3rd party API
  * @param onSelected will be called when user selects some item
- * @param rateLimiter `RateLimiter` instance to use for the queries, if `null` no rate limiting will be used and query will be performed one very user type
  */
 export async function itemsApiRequest(
     title: string,
     icon: string,
-    buildUrl: (text: string) => URL,
-    getCompletionItems: (json: any) => Item[] | null,
+    parserDefinition: ResourceParserDefinition,
     onSelected: (item: Item) => void,
-    rateLimiter: RateLimiter | null = null,
 ) {
+    const parser = new ResourceParser(parserDefinition)
     await ItemChooser.instance.show(
         title,
         icon,
         async (text: string) => {
             if (!text) return null
-            const url = buildUrl(text)
-            const response: Response | null = rateLimiter == null
-                ? await fetch(url)
-                : await rateLimiter.runWithRateLimiting(() => fetch(url))
-            if (!response) return null
-            return response.json().then(getCompletionItems)
+            return parser.parse(text)
         },
         onSelected,
-        rateLimiter == null
-            ? (() => {})
-            : (() => rateLimiter.discard())
+        () => parser.discard(),
     )
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import '@logseq/libs'
 import {ItemChooser} from './ItemChooser'
 import {itemsApiRequest} from './ItemsApi'
-import {insertItemAtCursor, parseMyAnimeListYear, tokenize} from './utils'
-import {RateLimiter} from './RateLimiter'
+import {insertItemAtCursor} from './utils'
 import {Item} from './Item'
+import {myAnimeListParserDefinition, imdbParserDefinition} from './ResourceParserDefinition'
 
 
 async function main() {
@@ -19,23 +19,10 @@ function registerMovieCommand() {
         'Movie (IMDB)', async () => {
             const block = await logseq.Editor.getCurrentBlock()
             if (!block) return
+
             await itemsApiRequest(
                 'Movie Title', icon,
-                (text: string) => {
-                    const query = tokenize(text)
-                    return new URL(`https://v2.sg.media-imdb.com/suggestion/${encodeURIComponent(query[0].toLowerCase())}/${encodeURIComponent(query)}.json`)
-                },
-                (json) => {
-                    return json['d'].map((d: any) => {
-                        if (!d.id) return null
-                        if (![ 'movie', 'tvSeries', 'tvMiniSeries'].includes(d.qid)) return null
-                        return {
-                            title: d.l,
-                            year: d.y,
-                            link: `https://www.imdb.com/title/${d.id}`
-                        }
-                    }).filterOutNullable()
-                },
+                imdbParserDefinition,
                 (item: Item) => {
                     insertItemAtCursor(item, icon, block.format)
                 }
@@ -45,7 +32,6 @@ function registerMovieCommand() {
 }
 
 function registerAnimeCommand() {
-    const rateLimiter = new RateLimiter(1)
     const icon = '🎬'
     logseq.Editor.registerSlashCommand(
         'Anime (MyAnimeList)', async () => {
@@ -54,17 +40,10 @@ function registerAnimeCommand() {
 
             await itemsApiRequest(
                 'Anime Title', icon,
-                (text: string) => {
-                    const query = encodeURIComponent(tokenize(text))
-                    return new URL(`https://api.jikan.moe/v4/anime?q=${query}&order_by=score&sort=desc`)
-                },
-                (json) => {
-                    return json['data'].map((d: any) => ({title: d.title, link: d.url, year: parseMyAnimeListYear(d.aired)}))
-                },
+                myAnimeListParserDefinition,
                 (item: Item) => {
                     insertItemAtCursor(item, icon, block.format)
                 },
-                rateLimiter,
             )
         },
     )

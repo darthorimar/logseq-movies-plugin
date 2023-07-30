8ce9eb57a78a7ad1e202d9b373109f831b0abec2
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-07-30 22:03:07 +0200

    Add tests for resource parsers

---
+++ test/resource.parser.test.ts
@@ -0,0 +1,48 @@
+import {ResourceParser} from '../src/ResourceParser'
+import {
+    imdbParserDefinition,
+    myAnimeListParserDefinition,
+    ResourceParserDefinition
+} from '../src/ResourceParserDefinition'
+import {expect} from 'chai'
+
+describe('imdb parser test', () => {
+    it('Should search for the movie by name', async () => {
+         await withParser(imdbParserDefinition, async parser => {
+             let result = await parser.parse('lord of the rings')
+             expect(result?.map(i => i.title)).to.contain.members(
+                 [
+                     'The Lord of the Rings: The Fellowship of the Ring',
+                     'The Lord of the Rings: The Two Towers',
+                 ]
+             )
+         })
+    })
+})
+
+describe('myAnimeList parser test', () => {
+    it('Should search for the anime by name', async () => {
+        await withParser(myAnimeListParserDefinition, async parser => {
+            let result = await parser.parse('naruto')
+            expect(result?.map(i => i.title)).to.contain.members(
+                [
+                    'Naruto: Shippuuden',
+                    'Naruto',
+                ]
+            )
+        })
+    })
+})
+
+async function withParser(
+    parserDescription: ResourceParserDefinition,
+    action: (parser: ResourceParser) => Promise<void>
+) {
+    let parser = new ResourceParser(parserDescription)
+    try {
+        await action(parser)
+
+    } finally {
+        parser.discard()
+    }
+}

a1cf8ba8c68eb0af8268b02cda74a013279ae7e5
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2023-09-03 17:20:51 +0200

    Refactor, reuse existing testdata for `applyTemplate` test

--- test/linkTemplates.test.ts
+++ test/linkTemplates.test.ts
@@ -1,11 +1,12 @@
 import assert from 'assert'
 import {applyLinkTemplate} from '../src/linkTemplates'
+import {movies} from './testData'
 
 describe('applyTemplate test', () => {
     it('Should replace a ${title} and ${year} to an actual value', () => {
         const actual = applyLinkTemplate(
             'Movie ${title}, year: ${year}, and again: ${title}, ${year}',
-            {title: 'Pulp Fiction', year: '1994', link: ''}
+            movies.pulpFiction
         )
         assert.equal(actual, 'Movie Pulp Fiction, year: 1994, and again: Pulp Fiction, 1994')
     })
